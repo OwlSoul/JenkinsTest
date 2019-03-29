97e5449ecf2777ded27f434344a9421da21bde05
Owl Automaton <OwlAutomaton@yandex.ru>
2019-03-30 00:07:24 +0300

    Release: 0.6.6

--- JKvYtgproPythonTest/python_release_test.py
+++ JKvYtgproPythonTest/python_release_test.py
@@ -2,7 +2,7 @@
 __author__ = "Yury D."
 __credits__ = ["Yury D.", "Some Dude", "Some Dudette"]
 __license__ = "MIT"
-__version__ = "0.6.5"
+__version__ = "0.6.6"
 __maintainer__ = "Yury D."
 __email__ = "SoulGate@yandex.ru"
 __status__ = "Alpha"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     # Chose the same as "name"
     packages = ['JKvYtgproPythonTest'],
     # Start with a small number and increase it with every change you make
-    version = '0.6.5',
+    version = '0.6.6',
     # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     license='MIT',
     # Give a short description about your library
@@ -18,7 +18,7 @@ setup(
     # Provide either the link to your github or to your website
     url = 'https://github.com/OwlSoul/JekninsTest',
     # I explain this later on
-    download_url = 'https://github.com/OwlSoul/JenkinsTest/archive/0.6.5.tar.gz',
+    download_url = 'https://github.com/OwlSoul/JenkinsTest/archive/0.6.6.tar.gz',
     # Keywords that define your package best
     keywords = ['Yandex', 'Transport', 'Masstransit', 'WebDriver'],
     # I get to this in a second
