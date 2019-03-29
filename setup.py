0c7f4c43f0a350274dbc99adf5d04abbde4a9ca5
Yury D <SoulGate@yandex.ru>
2019-03-29 14:12:23 +0300

    Release: 4.4.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     # Chose the same as "name"
     packages = ['JKvYtgproPythonTest'],
     # Start with a small number and increase it with every change you make
-    version = '',
+    version = '4.4.4',
     # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     license='MIT',
     # Give a short description about your library
